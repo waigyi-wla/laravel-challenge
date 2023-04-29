c8659701405b780cf43bf2fddbb9e40073b3db83
Wai Lwin Aung <you@example.com>
2023-04-29 10:29:44 +0630

    1. api/login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,56 +5,30 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\UserResource;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        if (! $request->email) {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            
+            $authUser = new UserResource(Auth::user()); 
+            
+            return response()->json([
+                'user' => $authUser,
+                'token' => $authUser->createToken('User-Token')->plainTextToken,
+            ]);
+        } 
+        else{ 
             return response()->json([
                 'status' => 422,
-                'message' => 'email is required',
-            ]);
-        }
+                'message' => "email or password is invalid"
+            ], 422);
+        } 
 
-        if (strlen($request->email) < 6) {
-            return response()->json([
-                'status' => 422,
-                'message' => 'email is invalid',
-            ]);
-        }
-
-        if (! $request->password) {
-            return response()->json([
-                'status' => 422,
-                'message' => 'password is required',
-            ]);
-        }
-        if (strlen($request->password) < 8) {
-            return response()->json([
-                'status' => 422,
-                'message' => 'password is invalid',
-            ]);
-        }
-
-        $user = User::where('email', $request->email)->first();
-        if (! $user) {
-            return response()->json([
-                'status' => 404,
-                'message' => 'Model not found.',
-            ]);
-        }
-
-        if (! Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'status' => 404,
-                'message' => 'Invalid credentials',
-            ]);
-        }
-
-        return response()->json([
-            'user' => $user,
-            'token' => $user->createToken('User-Token')->plainTextToken,
-        ]);
+        
     }
 }
+
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            "email_verified_at" => $this->email_verified_at->format('Y-m-d'),
+            'created_at' => $this->created_at->format('Y-m-d'),
+            'updated_at' => $this->updated_at->format('Y-m-d'),
+        ];
+    }
+}
